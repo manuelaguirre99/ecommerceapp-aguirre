07520fc8395af50892574a014fe7aef70118adcc
manuaguirre <jmanuel1999@hotmail.com>
2021-10-27 23:14:11 -0300

    subida de archivos

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import NavBar from "./components/NavBar/NavBar"
+import CartWidget from './components/NavBar/CartWidget';
+import ItemsContainer from './components/ItemListContainer/ItemListContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar>
+      <CartWidget/>
+      </NavBar>
+
+      <ItemsContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ItemsContainer = () => {
+    return(
+        <div className="container-fluid bg-light">
+            <h1>Items coming soon...</h1>
+
+        </div>
+    )
+}
+
+export default ItemsContainer
